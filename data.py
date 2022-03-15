1cf937b669c76a23f413fecbd58f9ca31bac9f31
Dorian3280 <verdondor@gmail.com>
2022-03-15 17:16:52 +0100

    wip getParams

--- data.py
+++ data.py
@@ -3,13 +3,31 @@ DATA = {
             "players": [
                 {
                     "name": "NEGATIVE MACRO",
+                    "country": "europe",
                     "puuid": "n85Ra5AOp8MkuVbg55ZO0PBR4s4XI5En_pFRVfPI1VV9wzaPMk7VcrryKbFpFLtMk0PYWOtar6FCtQ",
                 },
                 {
+                    "name": "Sorest",
+                    "country": "europe",
+                    "puuid": "o571y5yiCauzTiPCSUQNgCsPe3bdcqEAEd2E2GVlBfWUyevnEey9UUJeMZox27942seIkzgA17mxfw",
+                },
+                {
                     "name": "300 Ginz",
+                    "country": "europe",
                     "puuid": "hatnrxhQQIxTmKIEtEdlqTKnh4pwgAeS101WwfKw6UAbhU2gFnHwXVKmuFsJ0CWRb358XKEhx6Srpw",
                 },
+                {
+                    "name": "TTV SaskioLoL",
+                    "country": "americas",
+                    "puuid": "RlYlAJauKkypiN1KO--S2Bo2kP3zJhP2P5OoKDkqJmvyEwpbTfwpkWSbxivJyhZ478JLfHrppjHaGQ",
+                },
+                {
+                    "name": "이길생각만하기",
+                    "country": "asia",
+                    "puuid": "NI_NjPdvwAaBuBmBrxkDlojAon5FCDzRd5OCsubF1OaC_b4Imhq4HOaXVMpaSRvPhZJ56N0vmz5rnQ",
+                },
             ],
-            "columns": ['championName', 'gameDuration', 'kills', 'deaths', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
+            "role": "BOTTOM",
+            "columns": ['win', 'gameDuration', 'kills', 'deaths', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
         }, 
     }
