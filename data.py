5ed4bd5d2702a516a574f80adcea6e37f70ad835
Dorian3280 <verdondor@gmail.com>
2022-03-15 01:47:01 +0100

    do (get your score) & (ladder), wip (getParams)

---
+++ data.py
@@ -0,0 +1,15 @@
+DATA = {
+        "Vayne": {
+            "players": [
+                {
+                    "name": "NEGATIVE MACRO",
+                    "puuid": "n85Ra5AOp8MkuVbg55ZO0PBR4s4XI5En_pFRVfPI1VV9wzaPMk7VcrryKbFpFLtMk0PYWOtar6FCtQ",
+                },
+                {
+                    "name": "300 Ginz",
+                    "puuid": "hatnrxhQQIxTmKIEtEdlqTKnh4pwgAeS101WwfKw6UAbhU2gFnHwXVKmuFsJ0CWRb358XKEhx6Srpw",
+                },
+            ],
+            "columns": ['championName', 'gameDuration', 'kills', 'deaths', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
+        }, 
+    }
