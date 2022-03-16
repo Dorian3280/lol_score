2fc108b2fb2a15907ba9c45c5acd4a22cae1a6b2
Dorian3280 <verdondor@gmail.com>
2022-03-16 21:42:49 +0100

    done saveData, wip calcParams

--- data.py
+++ data.py
@@ -1,33 +1,69 @@
 DATA = {
-        "Vayne": {
-            "players": [
-                {
-                    "name": "NEGATIVE MACRO",
-                    "country": "europe",
-                    "puuid": "n85Ra5AOp8MkuVbg55ZO0PBR4s4XI5En_pFRVfPI1VV9wzaPMk7VcrryKbFpFLtMk0PYWOtar6FCtQ",
-                },
-                {
-                    "name": "Sorest",
-                    "country": "europe",
-                    "puuid": "o571y5yiCauzTiPCSUQNgCsPe3bdcqEAEd2E2GVlBfWUyevnEey9UUJeMZox27942seIkzgA17mxfw",
-                },
-                {
-                    "name": "300 Ginz",
-                    "country": "europe",
-                    "puuid": "hatnrxhQQIxTmKIEtEdlqTKnh4pwgAeS101WwfKw6UAbhU2gFnHwXVKmuFsJ0CWRb358XKEhx6Srpw",
-                },
-                {
-                    "name": "TTV SaskioLoL",
-                    "country": "americas",
-                    "puuid": "RlYlAJauKkypiN1KO--S2Bo2kP3zJhP2P5OoKDkqJmvyEwpbTfwpkWSbxivJyhZ478JLfHrppjHaGQ",
-                },
-                {
-                    "name": "이길생각만하기",
-                    "country": "asia",
-                    "puuid": "NI_NjPdvwAaBuBmBrxkDlojAon5FCDzRd5OCsubF1OaC_b4Imhq4HOaXVMpaSRvPhZJ56N0vmz5rnQ",
-                },
-            ],
-            "role": "BOTTOM",
-            "columns": ['win', 'gameDuration', 'kills', 'deaths', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
-        }, 
-    }
+    "Vayne": {
+        "players": [
+            {
+                "name": "NEGATIVE MACRO",
+                "country": "europe",
+                "puuid": "n85Ra5AOp8MkuVbg55ZO0PBR4s4XI5En_pFRVfPI1VV9wzaPMk7VcrryKbFpFLtMk0PYWOtar6FCtQ",
+            },
+            {
+                "name": "Sorest",
+                "country": "europe",
+                "puuid": "o571y5yiCauzTiPCSUQNgCsPe3bdcqEAEd2E2GVlBfWUyevnEey9UUJeMZox27942seIkzgA17mxfw",
+            },
+            {
+                "name": "300 Ginz",
+                "country": "europe",
+                "puuid": "hatnrxhQQIxTmKIEtEdlqTKnh4pwgAeS101WwfKw6UAbhU2gFnHwXVKmuFsJ0CWRb358XKEhx6Srpw",
+            },
+            {
+                "name": "TTV SaskioLoL",
+                "country": "americas",
+                "puuid": "RlYlAJauKkypiN1KO--S2Bo2kP3zJhP2P5OoKDkqJmvyEwpbTfwpkWSbxivJyhZ478JLfHrppjHaGQ",
+            },
+            {
+                "name": "이길생각만하기",
+                "country": "asia",
+                "puuid": "NI_NjPdvwAaBuBmBrxkDlojAon5FCDzRd5OCsubF1OaC_b4Imhq4HOaXVMpaSRvPhZJ56N0vmz5rnQ",
+            },
+        ],
+        "role": "BOTTOM",
+        "columns": ['win', 'gameDuration', 'kills', 'deaths', 'scoreCreeps', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
+    },
+    "Draven": {
+        "players": [
+            {
+                "name": "TyChee",
+                "country": "americas",
+                "puuid": "bTjX6A-q928ZIgLGOzuQ__rEnzOZzjkgxt9XLFtM9ULdefjfwG95TWXI2LEkh-iDkk8_VXcFbJ3lKw",
+            },
+            {
+                "name": "twitch tv NCXA",
+                "country": "europe",
+                "puuid": "4CmJC5nNFTXkAYtpQf2wCncpik9cOotn7VZT-dKDb3ZtEPEBADair7K1VNn8cXuesCEuaYY95BDr0A",
+            },
+            {
+                "name": "Dealersz",
+                "country": "europe",
+                "puuid": "LU70bd6VUMdvFohboLfQ1ln-Ikqlxgghk5Ru79EfWyI4TGIqN_tQSKAjjj-T4VXKSuPpQ_AkwW4W1g",
+            },
+            {
+                "name": "SWENZUUUUUUUUUUU",
+                "country": "europe",
+                "puuid": "eW8mLgDCWEbtspi13AYYdL1tl9Malwid_x1bwvggaS1LjlHwe94ay2sNJEjXBijQnwziw0wjPR7yYQ",
+            },
+            {
+                "name": "J God1",
+                "country": "asia",
+                "puuid": "8girLdSJoA967zX-QfU1_2dZMJSyJxeJzScqBlhN8-WzIF9mKYoIXsUZbLIRv0FYy3wNWh7SzypwJQ",
+            },
+        ],
+        "role": "BOTTOM",
+        "columns": ['win', 'gameDuration', 'kills', 'deaths', 'scoreCreeps', 'totalDamageDealtToChampions', 'totalMinionsKilled', 'neutralMinionsKilled']
+    },
+}
+
+OUTLIERS = ['gameEndedInEarlySurrender', 'gameEndedInSurrender', 'hadAfkTeammate']
+
+for i in DATA.values():
+    i["columns"] += OUTLIERS
---
+++ saveData.py
@@ -0,0 +1,102 @@
+import glob, os, time, requests, shutil, sys
+import pandas as pd
+from secret import API_KEY
+from data import DATA
+
+def getJsonFromlolApi(country, g):
+
+    """
+    Operation GET from the Riot Api, return all information about a game
+    country : where the game have been played
+    g : the id of the game
+    """
+
+    res = requests.get(f"https://{country}.api.riotgames.com/lol/match/v5/matches/{g}?api_key={API_KEY}")
+    if res.status_code != 200: raise Exception
+    return res.json()
+
+def getRecentGames(puuid, country, count):
+
+    """
+    Operation GET from the Riot Api, return all the recent games from a player
+    puuid : his id
+    country : from where he comes from
+    count : start index of games
+    """
+
+    res = requests.get(f"https://{country}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?type=ranked&start={count*20}&count=20&api_key={API_KEY}")
+    
+    if res.status_code != 200: raise Exception
+    return res.json()
+
+def getDFfromJson(champ, count):
+
+    """
+    Some operations to get all informations I get from API to Dataframes
+    Loop over players I get in data.py
+    Loop over their last games
+    Choose their games where they played the champ
+    Some formatting
+    Get into one dataframe
+    """
+
+    array = []
+    error = False
+    for p in DATA[champ]["players"]:
+        try:
+            games = getRecentGames(p["puuid"], p["country"], count)
+        except Exception:
+            print('games')
+            error = True
+            break
+        
+        for g in games:
+            try:
+                json = getJsonFromlolApi(p['country'], g)
+            except Exception:
+                print('game')
+                error = True
+                break
+            index = json['metadata']['participants'].index(p["puuid"])
+            player = json['info']['participants'][index]
+            if player['championName'] != champ: continue
+            if player['teamPosition'] != DATA[champ]["role"]: continue
+            try: player['hadAfkTeammate'] = player['challenges']['hadAfkTeammate']
+            except KeyError: player['hadAfkTeammate'] = 0
+            player['gameDuration'] = json['info']['gameDuration']
+            player['scoreCreeps'] = player['totalMinionsKilled'] + player['neutralMinionsKilled']
+            array.append(pd.DataFrame({x: [player[x]] for x in DATA[champ]["columns"]}))
+
+    return pd.concat(array).reset_index(drop=True), not error
+
+# Type the champpion name in command line after execution of the .py file - python saveDate.py [Champion name]
+champ = sys.argv[1].title()
+count = 0
+process = True
+
+# Create a directory to store datas
+try: os.mkdir(champ)
+except FileExistsError: pass
+
+# Riot API has limit request, can't exceed a certain amount of request per minute, so I has to slow down the execution
+while process and count != 5:
+    df, process = getDFfromJson(champ, count)
+    df.to_csv(f"{champ}/{count}.txt", index=False, header=(count==0))
+    count += 1
+    time.sleep(120)
+
+# Reassemble all datas file into a single one
+try:
+    filenames = [file for file in glob.glob(fr"{champ}/*")]
+    if not filenames:
+        raise Exception
+    with open(f'data/{champ}.txt', 'w') as outfile:
+        for fname in filenames:
+            with open(fname) as infile:
+                outfile.write(infile.read())
+
+except Exception:
+    print('Nothing...')
+
+# Delete trash
+shutil.rmtree(champ)
